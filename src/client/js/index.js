872d8a78bf89c676286f1f6ced9e2dfbcc8f4920
Khaled Saleh <saleh208@hotmail.com>
2021-03-08 03:14:43 +0200

    setup 3

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -3,14 +3,28 @@ import './../styles/main.scss';
 import './../styles/form.scss';
 import './../styles/header.scss';
 import './../styles/footer.scss';
-import {App,bootStrap} from './app';
+import {App} from './app';
 
 import logo from './../assets/nlp.png';
 
 
-
+/*
 export{
     bootStrap
 }
+*/
+export function bootStrap(){
+    console.log(`bootStrap() called........`);
+    if(window &&  window.Application!=null){
+        console.log(`Application already  exist ...`);
+        return ;
+    }
+
+        window.onload = function(){ 
+            console.log(`window onload ...`);
+            window.Application=new App();
+    }
+    console.log(`window onload registered  ...`);
+};
 
 bootStrap();
